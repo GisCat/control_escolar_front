8cc061490d9c4553874591765ab45046cd6690b4
GisCat <gisellmendez670@gmail.com>
2025-12-04 12:21:51 -0600

    d

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+// src/environments/environment.prod.ts
+
 export const environment = {
-  production: true
+  production: true,
+  // ¡CRUCIAL! Debe ser la URL de tu Web Service de Django
+  url_api: "https://control-escolar-back-dhku.onrender.com" 
 };
